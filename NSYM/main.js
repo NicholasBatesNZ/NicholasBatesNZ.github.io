dc68035f86eb75dd55b544766e67fbe60b435ce4
NicholasBatesNZ <nicholasbates.bates@gmail.com>
2017-07-23 15:32:07 +1200

    NSYM Website

---
+++ NSYM/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    /* $("main").css({
+        "top": -$("nav").outerHeight(true),
+        "margin-bottom": -$("nav").outerHeight(true)
+    }); */
+
+    /*$(window).scroll(function() {
+        if($(window).scrollTop() > $("header").offset().top + $("header").outerHeight()) {
+            $("nav").addClass("view");
+        }
+        else{
+            $("nav").removeClass("view");
+        }
+    });*/
+});
