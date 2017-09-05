c0e9cac322d9277402534c573b7be549233abc09
NicholasBatesNZ <nicholasbates.bates@gmail.com>
2017-09-05 12:44:34 +1200

    outline change

--- NSYM/main.js
+++ NSYM/main.js
@@ -1,16 +1,3 @@
-$(document).ready(function() {
-
-    /* $("main").css({
-        "top": -$("nav").outerHeight(true),
-        "margin-bottom": -$("nav").outerHeight(true)
-    }); */
-
-    /*$(window).scroll(function() {
-        if($(window).scrollTop() > $("header").offset().top + $("header").outerHeight()) {
-            $("nav").addClass("view");
-        }
-        else{
-            $("nav").removeClass("view");
-        }
-    });*/
+$(document).on("scroll", function() {
+    $("header, #headerBuffer").toggleClass("small", $(document).scrollTop() > 10);
 });
