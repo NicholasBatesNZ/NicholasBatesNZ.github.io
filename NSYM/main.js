5a01595169d07c3686aea8e4207b5e8ff5ae4bbc
NicholasBatesNZ <nicholasbates.bates@gmail.com>
2017-09-14 14:22:26 +1200

    Dropdown Start

--- NSYM/main.js
+++ NSYM/main.js
@@ -1,3 +1,10 @@
-$(document).on("scroll", function() {
-    $("header, #headerBuffer").toggleClass("small", $(document).scrollTop() > 10);
+$(document).ready(function() {
+
+    $(document).on("scroll", function() {
+        $("header, #headerBuffer").toggleClass("small", $(document).scrollTop() > 10);
+    });
+
+    $("nav span").hover(function() {
+        $(this).children("div").toggleClass("active");
+    });
 });
