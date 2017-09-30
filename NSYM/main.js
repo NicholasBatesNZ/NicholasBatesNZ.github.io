9ad55a0447be8c53242b19bf1fd0eea7eed5fe8b
NicholasBatesNZ <nicholasbates.bates@gmail.com>
2017-09-30 22:50:48 +1300

    Improved slideshow

--- NSYM/main.js
+++ NSYM/main.js
@@ -7,24 +7,25 @@ $(document).ready(function() {
     $("nav span").hover(function() {
         $(this).children("div").toggleClass("active");
     });
-
     $("nav span").click(function() {
         $(this).children("a")[0].click();
     });
 
-
+    var $images = $("#slideshow").children("img");
     var destination = 0;
-    $("#left.arrow").click(function() {
-        destination--;
-        updateSlide();
-    });
-    $("#right.arrow").click(function() {
-        destination++;
-        updateSlide();
-    });
+    createSlideDots();
+    function createSlideDots() {
+        var emsNeeded = 2 * $images.length - 1;
+        var emToPx = parseFloat($("body").css("font-size"));
+        var pxsSpareEachSide = ($(document).width() - emToPx * emsNeeded) / 2;
 
+        $images.each(function(index) {
+            $("#slideshow").append("<span class='dot' id='" + index + "'></span>");
+            $(".dot#" + index).css("left", pxsSpareEachSide + emToPx * (index * 2));
+            $(".dot#0").addClass("active");
+        });
+    }
     function updateSlide() {
-        var $images = $("#slideshow").children("img");
         
         if (destination > $images.length-1) {
             destination = 0;
@@ -35,11 +36,33 @@ $(document).ready(function() {
         
         $images.each(function(index) {
             if (index == destination) {
-                $(this).css("z-index", "1");
+                $(".dot#" + index).addClass("active");
+                $(this).hide().fadeIn(250, function() {
+                    $(this).css("z-index", "1");
+                });
             }
             else {
-                $(this).css("z-index", "-1");
+                $(".dot#" + index).removeClass("active");
+                $(this).fadeOut(250, function() {
+                    $(this).css("z-index", "-1").show();
+                });
             }
         });
     }
+    $("#left.arrow").click(function() {
+        destination--;
+        updateSlide();
+    });
+    $("#right.arrow").click(function() {
+        destination++;
+        updateSlide();
+    });
+    $(".dot").click(function() {
+        destination = $(this).attr("id");
+        updateSlide();
+    });
+    setInterval(function() {
+        destination++;
+        updateSlide();
+    }, 5000);
 });
