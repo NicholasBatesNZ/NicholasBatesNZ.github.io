3cc88d383abbeb52178c5fa16aad49e90402ec8d
NicholasBatesNZ <nicholasbates.bates@gmail.com>
2017-09-30 18:57:16 +1300

    Slideshow now functional

--- NSYM/main.js
+++ NSYM/main.js
@@ -11,4 +11,35 @@ $(document).ready(function() {
     $("nav span").click(function() {
         $(this).children("a")[0].click();
     });
+
+
+    var destination = 0;
+    $("#left.arrow").click(function() {
+        destination--;
+        updateSlide();
+    });
+    $("#right.arrow").click(function() {
+        destination++;
+        updateSlide();
+    });
+
+    function updateSlide() {
+        var $images = $("#slideshow").children("img");
+        
+        if (destination > $images.length-1) {
+            destination = 0;
+        }
+        if (destination < 0) {
+            destination = $images.length-1;
+        }
+        
+        $images.each(function(index) {
+            if (index == destination) {
+                $(this).css("z-index", "1");
+            }
+            else {
+                $(this).css("z-index", "-1");
+            }
+        });
+    }
 });
